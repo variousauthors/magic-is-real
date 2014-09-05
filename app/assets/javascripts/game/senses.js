0c99f4c379be975f095f89769eddfe56cefbcce9
Zeigfreid <various.authors@gmail.com>
2014-09-05 10:37:02 -0700

    The moons resize when clicked

--- app/assets/javascripts/game/senses.js
+++ app/assets/javascripts/game/senses.js
@@ -1,6 +1,8 @@
 // clicking on a sense increases its volume by one step
 
 $(document).ready(function () {
+    var moon_size = 50*Math.sqrt(2),
+        min_moon = moon_size/4.0;
 
     $(".senses > div").click(function sensesClickHandler (e) {
         var $this = $(e.currentTarget),
@@ -8,15 +10,17 @@ $(document).ready(function () {
             diameter = $moon.width(),
             container_width = $this.width();
 
-        if (diameter <= (50.0/8)) {
-            $moon.height(50);
-            $moon.width(50);
+        if (diameter === 0) {
+            diameter = moon_size;
+        } else if ((diameter) <= min_moon) {
+            diameter = 0;
         } else {
-            $moon.height(diameter/2);
-            $moon.width(diameter/2);
-
-            $moon.css({ left: container_width/2 - $moon.width()/2, top: container_width/2 - $moon.width()/2 });
+            diameter = diameter*0.6;
         }
+
+        $moon.height(diameter);
+        $moon.width(diameter);
+        $moon.css({ top: container_width/2 - diameter/2, left: container_width/2 - diameter/2 });
     });
 
 });
