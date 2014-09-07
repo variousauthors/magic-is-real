ca26bd9ec9701c677e7ef6297794aeb0fb94b617
Zeigfreid <various.authors@gmail.com>
2014-09-06 18:52:28 -0700

    Added the text, and the fading effect
    Also kind of scattered the text, but will probably do more later.

--- app/assets/javascripts/game/senses.js
+++ app/assets/javascripts/game/senses.js
@@ -2,14 +2,16 @@
 
 $(document).ready(function () {
     var moon_size = 50*Math.sqrt(2),
-        min_moon = moon_size/4.0;
+        min_moon = moon_size/4.0,
+        resizeMoon, fadeStimuli,
+        $scenes = $(".scenes");
 
-    $(".senses > div").click(function sensesClickHandler (e) {
-        var $this = $(e.currentTarget),
-            $moon = $this.find(".moon"),
-            diameter = $moon.width(),
-            container_width = $this.width();
+    // TODO container_width could be a constant
+    resizeMoon = function resizeMoon ($moon, $container) {
+        var diameter = $moon.width(),
+            container_width = $container.width();
 
+        // resize the sense moon
         if (diameter === 0) {
             diameter = moon_size;
         } else if ((diameter) <= min_moon) {
@@ -21,6 +23,33 @@ $(document).ready(function () {
         $moon.height(diameter);
         $moon.width(diameter);
         $moon.css({ top: container_width/2 - diameter/2, left: container_width/2 - diameter/2 });
+
+        // we need to return the diameter because the animations are async
+        return diameter;
+    };
+
+    fadeStimuli = function fadeStimuli ($stimuli, ratio) {
+        $stimuli.find(".stimulus").css({ opacity: ratio });
+
+        // TODO if a stimulus is not 100 opaque, its links should not work. (how to do this)
+    };
+
+    $(".senses > div").click(function sensesClickHandler (e) {
+        var $this = $(e.currentTarget),
+            $moon = $this.find(".moon"),
+            sense = $moon.parent().attr("class"),
+            $stimuli = $scenes.find("." + sense),
+            diameter;
+
+        diameter = resizeMoon($moon, $this);
+
+        fadeStimuli($stimuli, diameter / $this.width());
     });
 
+    // scramble the stimuli
+    $(".stimulus").each(function (index, stimulus) {
+        var width = $scenes.width();
+
+        $(stimulus).css({ top: Math.random() * width, left: Math.random() * width - 50 });
+    });
 });
