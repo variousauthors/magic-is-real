629a17bf3855e0d32f1a0d539fbb41462c4bad17
Zeigfreid <various.authors@gmail.com>
2014-09-13 16:36:23 -0700

    [WIP] Clicking links ajax loads a passage

--- app/assets/javascripts/game/stimuli.js
+++ app/assets/javascripts/game/stimuli.js
@@ -1,10 +1,22 @@
 $(function () {
-    var scramble;
+    var scramble, render, renderCollection;
 
     scramble = function scramble (index, stimulus) {
         var width = $scenes.width();
 
         $(stimulus).css({ top: Math.random() * width, left: Math.random() * width - 50 });
+    };
+
+    render = function render (sense, stimulus) {
+        var $stimulus = $("<div>").addClass("stimulus");
+
+        $stimulus.html(stimulus.content);
+        $scenes.find('.' + sense).append($stimulus);
+    };
+
+    renderCollection = function renderCollection (stimuli) {
+        $.each(stimuli, render);
+        $stimuli.each(scramble);
     };
 
     // scramble the stimuli
@@ -14,6 +26,11 @@ $(function () {
         e.preventDefault();
 
         // fade out, ajax request, then scramble, then fade in
+        $.ajax({
+            url: "passages/1",
+            dataType: "json",
+            success: renderCollection
+        });
     });
 
     $stimuli.on("click", "a.detail", function (e) {
